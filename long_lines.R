47b7bc246ccb35b77a32e759e16d7704df01a098
Fernando Roa O <fernando@appsilon.com>
2022-11-06 20:59:46 -0300

    fix: edge cases, simplify code

--- long_lines.R
+++ long_lines.R
@@ -3,13 +3,18 @@ iris %>%
   summarise(Sepal.Length = mean(Sepal.Length), Sepal.Width = mean(Sepal.Width), Species = n_distinct(Species))
 
 #' short comment, do nothing starting in #'
-#' long starting in #'  line to split big chunk line to split big chunk, HERE line to split big chunk line to split big chunk line to split
+#' long starting with #'  line  big chunk, line  big chunk HERE line  big chunk line  big chunk line long starting, with #' long starting with #'
 
 #' ruler10_234567_20_234567_30_234567_40_234567_50_234567_60_234567_70_234567_80_234567_90_23456_100
 #'
 #' short comment, previous line do nothing
 
 # only comments starting #
-# long comments starting # comments only comments only comments only comments only, HERE comments only comments only comments only comments only comments
+# long comments starting with # comments only comments only comments only comments only, HERE comments only comments only comments only comments only comments
+# BIG comment line no commas line  BIG comment line no commas line  BIG comment line no commas line  BIG comment line no commas line  BIG comment line no commas line
 
-code(arg1, arg2) # code and then comments then comments then comments then comments then, comments then comments then commentsthen comments
+code(arg1, arg2) # code and then comments then comments then comments then comments then, comments then comments then comments then comments
+
+BIG no commas line BIG no commas line BIG no commas line BIG no commas line BIG no commas line BIG no commas line BIG no commas line
+#' BIG comment line no commas line  BIG comment line no commas line  BIG comment line no commas line  BIG comment line no commas line  BIG comment line no commas line
+BIG no commas line BIG no commas line BIG no commas line BIG no commas line BIG no commas line BIG no commas line BIG no commas line
--- wrapper.pl
+++ wrapper.pl
@@ -1,41 +1,39 @@
 #!/usr/bin/perl
 my $numArgs = $#ARGV + 1;
 
-$max_length = $ARGV[0];                   q{ 1st idx 0 = length };
+$max_length = $ARGV[0];                   # 1st idx 0 = length of line
 
 $ARGV[1] =~ s/\\s/ /g;
 $ARGV[1] =~ s/\\//g;
-$line_filter_pattern = qr/$ARGV[1]/;      q{ 2nd idx 1 = filter_pattern, when to act };
+$line_filter_pattern = qr/$ARGV[1]/;      # 2nd idx 1 = filter_pattern, when to act
 
 $ARGV[2] =~ s/\\s/ /g;
 $ARGV[2] =~ s/\\//g;
-$prefix_pattern = $ARGV[2];               q{ 3rd idx 2 = prefix_pattern, which prefix to add };
+$prefix_pattern = $ARGV[2];               # 3rd idx 2 = prefix_pattern, which prefix to add
 
-$break_point = $ARGV[3];                  q{ 4rd idx 3 = character in which to break lines };
+$break_point = $ARGV[3];                  # 4rd idx 3 = character in which to break lines
 
-
-$linebreak_prefix = "\n$prefix_pattern";  q{ \n is linebreak };
+$linebreak_prefix = "\n$prefix_pattern";  # \n is linebreak
 $li_pre_length = length($linebreak_prefix);
 $max_length -= ($li_pre_length - 1);
 
 foreach my $line ( <STDIN> ) {
 
-q{act in lines with prefix pattern if else };
+# act in lines with prefix pattern if
   if ($line =~ /$line_filter_pattern.*?$/) {
-      ($r2 = $r = $line);
+      $r2 = $line;
+      $l_line = length($line);
 
-q{    check if splitting is necessary if else };
-      $r2 =~ s/\G.{0,$max_length}($break_point|.$)\K/$linebreak_prefix/gsem;
-      if(length($r) != $max_length + $li_pre_length and length($r2) != length($r) + $li_pre_length ) {
+#     Check if splitting is necessary/possible
+      if ($line =~ /$break_point/) {
+        $r2 =~ s/\G.{0,$max_length}($break_point|.$)\K/$linebreak_prefix/gsem;
 
-q{      add breaks and prefixes in a loop way };
-        $r = $r2;
-        $r =~ s/$linebreak_prefix$//;
-        print $r;
-      } else {
-        print $line;
+        if(not($l_line == $max_length + $li_pre_length or length($r2) == $l_line + $li_pre_length) ) {
+#         add breaks and prefixes in a loop way as r2 };
+          $line = $r2;
+          $line =~ s/$linebreak_prefix$//;
+        }
       }
-  } else {
-    print $line;
   }
+  print $line;
 }
